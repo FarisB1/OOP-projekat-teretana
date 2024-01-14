498323265b5c897a7b29515a99d3e42de7a1529a
FarisB1 <124277218+FarisB1@users.noreply.github.com>
2024-01-14 16:59:19 +0100

    Update Datum.h

--- Datum.h
+++ Datum.h
@@ -35,41 +35,4 @@ public:
     bool operator>(const Datum& other) const;
 };
 
-
-#endif //PROJEKAT_DATUM_H
-
-class Datum {
-private:
-    int dan;
-    int mjesec;
-    int godina;
-public:
-    void setDan();
-    void setMjesec();
-    void setGodina();
-
-    void setDan(int a);
-    void setMjesec(int a);
-    void setGodina(int a);
-
-
-    int getDan();
-    int getMjesec();
-    int getGodina();
-
-    void postaviDatum();
-    void ispisiDatum();
-    std::string ispisiDatum2();
-    int daysInMonth(int month, int year) const;
-
-
-    Datum(int day, int month, int year);
-    Datum();
-    ~Datum(){};
-
-    Datum operator+(int days) const;
-    bool operator>(const Datum& other) const;
-};
-
-
 #endif //PROJEKAT_DATUM_H
